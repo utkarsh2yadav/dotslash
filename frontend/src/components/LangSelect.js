d0fb1d0d347a8410af5a0be6b625023f5cd66c37
utkarsh2yadav <utkarshyadav256@gmail.com>
2022-03-24 21:38:10 +0530

    frontend
    basic frontend design

---
+++ frontend/src/components/LangSelect.js
@@ -0,0 +1,18 @@
+import {FormControl, Select, MenuItem, InputLabel} from '@mui/material';
+export default function LangSelect() {
+  return <FormControl size='medium'>
+    <InputLabel id="language">Age</InputLabel>
+    <Select
+      labelId="language"
+      id="language"
+      
+      label="Language"
+    
+    >
+      <MenuItem value={'Java'}>Java</MenuItem>
+      <MenuItem value={'Javascript'}>Javascript</MenuItem>
+      <MenuItem value={'Python'}>Python</MenuItem>
+      <MenuItem value={'Golang'}>Golang</MenuItem>
+    </Select>
+  </FormControl>
+}
