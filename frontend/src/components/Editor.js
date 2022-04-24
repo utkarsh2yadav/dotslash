5c3703679d4a0d9e3d8346a049f9d29a7f8597f6
Aman Sinha <aksinha.101010@gmail.com>
2022-04-25 02:02:16 +0530

    add mode and extensions and change theme for editor

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-golang";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/ext-language_tools";
-import { Fab } from "@mui/material";
-import { PlayArrow } from "@mui/icons-material";
+import "ace-builds/src-noconflict/mode-c_cpp"
+import "ace-builds/src-noconflict/mode-golang"
+import "ace-builds/src-noconflict/mode-java"
+import "ace-builds/src-noconflict/mode-javascript"
+import "ace-builds/src-noconflict/mode-python"
+import "ace-builds/src-noconflict/mode-typescript"
+import "ace-builds/src-noconflict/theme-one_dark"
+
+import "ace-builds/src-noconflict/ext-language_tools"
+import beautify from "ace-builds/src-noconflict/ext-beautify"
+
+import "ace-builds/src-noconflict/snippets/c_cpp"
+import "ace-builds/src-noconflict/snippets/golang"
+import "ace-builds/src-noconflict/snippets/java"
+import "ace-builds/src-noconflict/snippets/javascript"
+import "ace-builds/src-noconflict/snippets/python"
+import "ace-builds/src-noconflict/snippets/typescript"
 
 
 export default function Editor(props) {
@@ -20,13 +32,15 @@ export default function Editor(props) {
       width="100%"
       height="100%"
       mode="golang"
-      theme="monokai"
+      theme="one_dark"
       name="editor"
       editorProps={{ $blockScrolling: true }}
       onChange={(value) => {
         setCode(value)
       }}
+      commands={beautify.commands}
     />
+    Ctrl + Shift + B to beautify
     <Fab color='secondary' variant="extended" onClick={(_) => {
       let ws = new WebSocket("ws://localhost:8080/ws/golang")
       ws.onopen = () => {
